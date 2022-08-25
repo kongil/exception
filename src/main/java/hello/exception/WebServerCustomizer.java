33bdb5f841a723ba2bdc77c2e05e282ecbedc860
kongil <o_o0122@naver.com>
2022-08-25 23:51:53 +0900

    API 예외 처리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
@@ -15,6 +15,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
 
         ErrorPage errorPageEx= new ErrorPage(RuntimeException.class, "/error-page/500");
+
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
 }
